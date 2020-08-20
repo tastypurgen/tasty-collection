fd4f554efec99637bd7f86170a01f3d705e5b168
Andrei Abramchuk <stilegs@gmail.com>
2020-08-20 22:14:32 +0300

    add header translation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,29 +5,48 @@ import {
   Redirect,
   Switch,
 } from 'react-router-dom';
+import { IntlProvider } from 'react-intl';
 
 import Users from './user/pages/Users';
 import NewItem from './items/pages/NewItem';
 import Header from './shared/Header';
 import UserItems from './items/pages/UserItems';
+import en from './shared/localization/en.json';
+import ru from './shared/localization/ru.json';
+import locales from './shared/localization/locales';
+
+const messages = {
+  [locales.EN]: en,
+  [locales.RU]: ru,
+};
+
+const defaultLocale = locales.RU;
 
 export default function App() {
   return (
     <Router>
-      <Header />
 
-      <Switch>
-        <Route exact path="/">
-          <Users />
-        </Route>
-        <Route exact path="/:userId/items">
-          <UserItems />
-        </Route>
-        <Route exact path="/items/add">
-          <NewItem />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
+      <IntlProvider
+        messages={messages[defaultLocale]}
+        locale={defaultLocale}
+        defaultLocale={defaultLocale}
+      >
+        <Header />
+
+        <Switch>
+          <Route exact path="/">
+            <Users />
+          </Route>
+          <Route exact path="/:userId/items">
+            <UserItems />
+          </Route>
+          <Route exact path="/items/add">
+            <NewItem />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+
+      </IntlProvider>
     </Router>
   );
 }
--- frontend/src/shared/Header.jsx
+++ frontend/src/shared/Header.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import {
-  Navbar, Nav, Form, FormControl, Button,
+  Navbar, Nav, Form, FormControl, Button, Image,
 } from 'react-bootstrap';
 import { NavLink, Link } from 'react-router-dom';
+import { useIntl, FormattedMessage } from 'react-intl';
+
+const assetsPath = `${process.env.PUBLIC_URL}/assets`;
 
 export default function Header() {
+  const intl = useIntl();
+
   return (
     <Navbar className="mb-3" bg="dark" variant="dark" expand="md">
       <Link to="/">
-        <Navbar.Brand>TastyCollection</Navbar.Brand>
+        <Navbar.Brand>
+          <Image src={`${assetsPath}/logo.png`} style={{ width: '150px' }} />
+        </Navbar.Brand>
       </Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link as={NavLink} exact to="/">Home</Nav.Link>
-          <Nav.Link as={NavLink} to="/u1/items">My Items</Nav.Link>
-          <Nav.Link as={NavLink} to="/items/add">Add Item</Nav.Link>
+          <Nav.Link as={NavLink} exact to="/">
+            <FormattedMessage id="header.home" />
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/u1/items">
+            <FormattedMessage id="header.myItems" />
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/items/add">
+            <FormattedMessage id="header.addItem" />
+          </Nav.Link>
         </Nav>
         <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-info">Search</Button>
+          <FormControl type="text" placeholder={intl.formatMessage({ id: 'header.searchPlaceholder' })} className="mr-sm-2" />
+          <Button variant="outline-info">
+            <FormattedMessage id="header.searchBtn" />
+          </Button>
         </Form>
       </Navbar.Collapse>
     </Navbar>
