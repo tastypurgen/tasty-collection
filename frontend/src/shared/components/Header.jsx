9a09cbedae11ca655dc1737c665ccdf7dcf48d02
Andrei Abramchuk <stilegs@gmail.com>
2020-09-24 22:07:24 +0300

    add basic

--- frontend/src/shared/components/Header.jsx
+++ frontend/src/shared/components/Header.jsx
@@ -4,14 +4,35 @@ import {
 } from 'react-bootstrap';
 import { NavLink, Link } from 'react-router-dom';
 import { useIntl, FormattedMessage } from 'react-intl';
+import algoliasearch from 'algoliasearch';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
 
 import { AuthContext } from '../context/AuthContext';
+
+const searchClient = algoliasearch(
+  'VJEOP80F3M',
+  '336ba11a72c860a2fa9b2a538a23f4c3',
+);
 
 const assetsPath = `${process.env.PUBLIC_URL}/assets`;
 
 export default function Header() {
   const auth = useContext(AuthContext);
   const intl = useIntl();
+
+  const client = algoliasearch('VJEOP80F3M', '75c59832e8dc4d2176548753cd95d090');
+
+  const index = client.initIndex('tasty-collection-test');
+
+  fetch(`${process.env.REACT_APP_BACKEND_URL}/items/all`)
+    .then((response) => response.json())
+    .then((items) => {
+      index.clearObjects();
+      return items;
+    })
+    .then(({ items }) => index.saveObjects(items, {
+      autoGenerateObjectIDIfNotExist: true,
+    }));
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
@@ -20,6 +41,14 @@ export default function Header() {
           <Image src={`${assetsPath}/logo.png`} style={{ width: '150px' }} />
         </Navbar.Brand>
       </Link>
+
+      <InstantSearch
+        indexName="tasty-collection-test"
+        searchClient={searchClient}
+      >
+        <SearchBox />
+        <Hits />
+      </InstantSearch>
 
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
