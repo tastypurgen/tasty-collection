f481ec572b737b8b80b22d5b45ede947d5f9f689
Andrei Abramchuk <stilegs@gmail.com>
2020-09-25 21:56:45 +0300

    implement item search

--- frontend/src/shared/components/Header.jsx
+++ frontend/src/shared/components/Header.jsx
@@ -1,38 +1,18 @@
 import React, { useContext } from 'react';
 import {
-  Navbar, Nav, Form, FormControl, Button, Image,
+  Navbar, Nav, Button, Image,
 } from 'react-bootstrap';
 import { NavLink, Link } from 'react-router-dom';
-import { useIntl, FormattedMessage } from 'react-intl';
-import algoliasearch from 'algoliasearch';
-import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+import { FormattedMessage } from 'react-intl';
 
+import './Header.scss';
 import { AuthContext } from '../context/AuthContext';
-
-const searchClient = algoliasearch(
-  'VJEOP80F3M',
-  '336ba11a72c860a2fa9b2a538a23f4c3',
-);
+import Search from './Search';
 
 const assetsPath = `${process.env.PUBLIC_URL}/assets`;
 
 export default function Header() {
   const auth = useContext(AuthContext);
-  const intl = useIntl();
-
-  const client = algoliasearch('VJEOP80F3M', '75c59832e8dc4d2176548753cd95d090');
-
-  const index = client.initIndex('tasty-collection-test');
-
-  fetch(`${process.env.REACT_APP_BACKEND_URL}/items/all`)
-    .then((response) => response.json())
-    .then((items) => {
-      index.clearObjects();
-      return items;
-    })
-    .then(({ items }) => index.saveObjects(items, {
-      autoGenerateObjectIDIfNotExist: true,
-    }));
 
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
@@ -41,14 +21,6 @@ export default function Header() {
           <Image src={`${assetsPath}/logo.png`} style={{ width: '150px' }} />
         </Navbar.Brand>
       </Link>
-
-      <InstantSearch
-        indexName="tasty-collection-test"
-        searchClient={searchClient}
-      >
-        <SearchBox />
-        <Hits />
-      </InstantSearch>
 
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
@@ -66,16 +38,7 @@ export default function Header() {
           </Nav>
         )}
 
-        <Form className="mr-auto" inline>
-          <FormControl
-            type="text"
-            placeholder={intl.formatMessage({ id: 'Header.SearchPlaceholder' })}
-            className="mr-sm-2"
-          />
-          <Button variant="outline-info">
-            <FormattedMessage id="Header.SearchBtn" />
-          </Button>
-        </Form>
+        <Search />
 
         {!auth.isLoggedIn && (
           <div>
---
+++ frontend/src/shared/components/Search.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+import { InstantSearch, SearchBox, Hits } from 'react-instantsearch-dom';
+import algoliasearch from 'algoliasearch';
+import { Link } from 'react-router-dom';
+
+const searchClient = algoliasearch(
+  process.env.REACT_APP_ALGOLIA_ID,
+  process.env.REACT_APP_ALGOLIA_KEY,
+);
+
+const client = algoliasearch(process.env.REACT_APP_ALGOLIA_ID, process.env.REACT_APP_ALGOLIA_KEY);
+
+const index = client.initIndex('tasty-collection-test');
+
+fetch(`${process.env.REACT_APP_BACKEND_URL}/items/all`)
+  .then((response) => response.json())
+  .then((items) => {
+    index.clearObjects();
+    return items;
+  })
+  .then(({ items }) => index.saveObjects(items, {
+    autoGenerateObjectIDIfNotExist: true,
+  }));
+
+export default function Search() {
+  const [searchInput, setSearchInput] = useState('');
+
+  const Hit = ({ hit }) => (
+    <Link to={`/items/${hit.id}`} onClick={() => setSearchInput('')}>
+      <div className="hit">
+        <div className="hit-image">
+          <img style={{ width: '50%' }} src={hit.image} alt="" />
+        </div>
+        <div className="hit-content">
+          <div className="hit-name">
+            {hit.title}
+          </div>
+          <div className="hit-desc">
+            {hit.description}
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+
+  const handleSearch = (event) => {
+    setSearchInput(event.target.value);
+  };
+  return (
+
+    <InstantSearch
+      indexName={process.env.REACT_APP_ALGOLIA_NAME}
+      searchClient={searchClient}
+    >
+      <SearchBox
+        translations={{ placeholder: '...' }}
+        onChange={handleSearch}
+        value={searchInput}
+      />
+      {searchInput && <Hits hitComponent={Hit} />}
+    </InstantSearch>
+  );
+}
